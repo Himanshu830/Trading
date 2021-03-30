701e30576747dfddd0ce42f0f83c3c98d3e5e73c
Ranjan Kumar <ranjanjha111@gmail.com>
2021-03-30 15:58:23 +0530

    Add linkedin and twitter share

--- src/components/product/ViewProduct.js
+++ src/components/product/ViewProduct.js
@@ -4,10 +4,17 @@ import { getProductById } from './api';
 import { SuccessMessage, ErrorMessage } from '../message/messages';
 import { Loader } from '../loader/loader';
 import { arrayBufferToBase64 } from '../../utility/image';
-import { FacebookShareButton, FacebookIcon} from "react-share";
 import { SITE_URL } from '../../config';
 import Menu from '../layout/partial/Menu';
 import { isAuthenticated } from '../auth/api';
+import { 
+    FacebookShareButton,
+    LinkedinShareButton,
+    TwitterShareButton,
+    FacebookIcon,
+    LinkedinIcon,
+    TwitterIcon,
+    } from "react-share";
 
 class ViewProduct extends Component {
     state = {
@@ -28,7 +35,7 @@ class ViewProduct extends Component {
     productId = this.props.match.params.productId;
 
     componentDidMount() {
-        this.setState({loading: true});
+        this.setState({ loading: true });
         getProductById(this.productId).then(product => {
             let data = product.result;
             this.setState({
@@ -45,7 +52,7 @@ class ViewProduct extends Component {
             });
         }).catch(error => {
             console.log(error)
-            this.setState({loading: false, error: 'Product not found.'})
+            this.setState({ loading: false, error: 'Product not found.' })
         })
     }
 
@@ -68,7 +75,7 @@ class ViewProduct extends Component {
 
         const { user } = isAuthenticated();
 
-        if(user) {
+        if (user) {
             return (
                 <Layout>
                     <div className="col-sm-9">
@@ -93,19 +100,18 @@ class ViewProduct extends Component {
                                 <li className="list-group-item"><b>Delivery Time</b>: {deliveryTime}</li>
                                 <li className="list-group-item"><b>Image</b>:
                                 {image && (
-                                    <img
-                                        height="75px"
-                                        src={`data:${image.contentType};base64,${arrayBufferToBase64(image.data.data)}`}
-                                        alt='product'
-                                    />
-                                )}
+                                        <img
+                                            height="75px"
+                                            src={`data:${image.contentType};base64,${arrayBufferToBase64(image.data.data)}`}
+                                            alt='product'
+                                        />
+                                    )}
                                 </li>
-                                <li className="list-group-item"><b>Share</b>:
-                                    <FacebookShareButton 
-                                        url={`${SITE_URL}/product/${this.productId}`}
-                                    >
-                                        <FacebookIcon size={36} />
-                                    </FacebookShareButton>    
+                                <li className="list-group-item">
+                                    <div className="row">
+                                        <div className="col-sm-1"><b>Share</b>:</div>
+                                        { this.socialShareButton() }
+                                    </div>
                                 </li>
                             </ul>
                         </div>
@@ -115,52 +121,128 @@ class ViewProduct extends Component {
         } else {
             return (
                 <Fragment>
-                    <div className="row" style={{marginTop: "20px"}}>
-                    <div className="col-sm-2"></div>
-                    <div className="col-sm-8">
-                        <div className="card">
-                            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-                                        <li className="nav-item active">
-                                            <span className="nav-link" to="/product"><strong>Product Detail</strong></span>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </nav>
-                            <ul className="list-group">
-                                <li className="list-group-item"><b>Name</b>: {name}</li>
-                                <li className="list-group-item"><b>Category</b>: {categoryName}</li>
-                                <li className="list-group-item"><b>Subcategory</b>: {subCategoryName}</li>
-                                <li className="list-group-item"><b>Description</b>: {description}</li>
-                                <li className="list-group-item"><b>Unit Price</b>: {unitPrice}</li>
-                                <li className="list-group-item"><b>Min Quantity</b>: {minQuantity}</li>
-                                <li className="list-group-item"><b>Packaging Details</b>: {packagingDetail}</li>
-                                <li className="list-group-item"><b>Delivery Time</b>: {deliveryTime}</li>
-                                <li className="list-group-item"><b>Image</b>:
-                                {image && (
-                                    <img
-                                        height="75px"
-                                        src={`data:${image.contentType};base64,${arrayBufferToBase64(image.data.data)}`}
-                                        alt='product'
-                                    />
-                                )}
-                                </li>
-                                <li className="list-group-item"><b>Share</b>:
-                                    <FacebookShareButton 
-                                        url={`${SITE_URL}/product/${this.productId}`}
-                                    >
-                                        <FacebookIcon size={36} />
-                                    </FacebookShareButton>    
-                                </li>
-                            </ul>
+                    <Menu />
+                    <div className="row" style={{ marginTop: "20px" }}>
+                        <div className="col-sm-2"></div>
+                        <div className="col-sm-8">
+                            <div className="card">
+                                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                                    <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                                        <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                                            <li className="nav-item active">
+                                                <span className="nav-link" to="/product"><strong>Product Detail</strong></span>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                                <ul className="list-group">
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Name</b>:</div>
+                                            <div className="col-sm-6">{name}</div>
+                                        </div>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Category</b>:</div>
+                                            <div className="col-sm-6">{categoryName}</div>
+                                        </div>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Subcategory</b>:</div>
+                                            <div className="col-sm-6">{subCategoryName}</div>
+                                        </div>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Description</b>:</div>
+                                            <div className="col-sm-6">{description}</div>
+                                        </div>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Unit Price</b>:</div>
+                                            <div className="col-sm-6">{unitPrice}</div>
+                                        </div>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Min Quantity</b>:</div>
+                                            <div className="col-sm-6">{minQuantity}</div>
+                                        </div>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Packaging Detail</b>:</div>
+                                            <div className="col-sm-6">{packagingDetail}</div>
+                                        </div>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Delivery Time</b>:</div>
+                                            <div className="col-sm-6">{deliveryTime}</div>
+                                        </div>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Image</b>:</div>
+                                            <div className="col-sm-6">
+                                            {image && (
+                                                <img
+                                                    height="75px"
+                                                    src={`data:${image.contentType};base64,${arrayBufferToBase64(image.data.data)}`}
+                                                    alt='product'
+                                                />
+                                            )}
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li className="list-group-item">
+                                        <div className="row">
+                                            <div className="col-sm-2"><b>Share</b>:</div>
+                                            { this.socialShareButton() }
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
+                        <div className="col-sm-2"></div>
                     </div>
-                    <div className="col-sm-2"></div>
-                </div>
                 </Fragment>
             );
         }
+    }
+
+    
+    socialShareButton = () => {
+        return (
+            <Fragment>
+                <div className="col-sm-1">
+                    <FacebookShareButton
+                        url={`${SITE_URL}/product/${this.productId}`}
+                    >
+                        <FacebookIcon size={36} />
+                    </FacebookShareButton>
+                </div>
+
+                <div className="col-sm-1">
+                    <LinkedinShareButton
+                        url={`${SITE_URL}/product/${this.productId}`}
+                    >
+                        <LinkedinIcon size={36} />
+                    </LinkedinShareButton>
+                </div>
+
+                <div className="col-sm-1">
+                    <TwitterShareButton
+                        url={`${SITE_URL}/product/${this.productId}`}
+                    >
+                        <TwitterIcon size={36} />
+                    </TwitterShareButton>
+                </div>
+            </Fragment>
+        )
     }
 
     render() {
